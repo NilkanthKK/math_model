c990d8a332191b7fa47fc1e90fec6d434e955c9c
NilkanthKK <nilkanth.koffeekodes@gmail.com>
2025-09-09 10:46:31 +0530

    add content in main

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("======================Heloo World======================")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World from Flask on Render!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
